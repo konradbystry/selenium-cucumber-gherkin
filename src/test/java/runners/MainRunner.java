178d17418f659de4dc0a7b7986f3e2834b10a989
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2023-12-04 23:45:17 +0100

    added tag

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -3,6 +3,7 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"}, monochrome = false, dryRun = false)
+@CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
+        tags = "@contact-us", monochrome = false, dryRun = false)
 public class MainRunner extends AbstractTestNGCucumberTests {
 }
