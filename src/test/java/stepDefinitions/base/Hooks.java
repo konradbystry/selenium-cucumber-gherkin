a0a98dfc6f3a12adff9733b3d6e329a20eb7c677
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2023-12-06 12:25:50 +0100

    implemented driver factory in hooks

--- src/test/java/stepDefinitions/base/Hooks.java
+++ src/test/java/stepDefinitions/base/Hooks.java
@@ -2,29 +2,18 @@ package stepDefinitions.base;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.PageLoadStrategy;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import static driver.DriverFactory.cleanupDriver;
+import static driver.DriverFactory.getDriver;
 
 public class Hooks {
 
-    public WebDriver driver;
-
     @Before
     public void setup(){
-        //System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/drivers/chromedriver.exe");
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        chromeOptions.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(chromeOptions);
-        driver.manage().window().maximize();
+        getDriver();
     }
 
     @After
     public void tearDown(){
-        driver.quit();
+        cleanupDriver();
     }
 }
