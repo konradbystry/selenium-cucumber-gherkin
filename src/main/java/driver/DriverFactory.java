a425428881c66321c5687938d7835a029206c28f
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2023-12-06 12:44:04 +0100

    implemented loading properties to get browser

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -10,6 +10,10 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class DriverFactory {
     private static ThreadLocal<WebDriver> webDriverThreadLocal = new ThreadLocal<>();
 
@@ -26,7 +30,7 @@ public class DriverFactory {
 
         String browserType = "chrome";
 
-        switch (browserType){
+        switch (getBrowserType()){
             case "chrome" -> {
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
@@ -54,6 +58,21 @@ public class DriverFactory {
         return driver;
     }
 
+
+    private static String getBrowserType() {
+        String browserType = null;
+
+        try {
+            Properties properties = new Properties();
+            FileInputStream fileInputStream = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/properties/config.properties");
+            properties.load(fileInputStream);
+            browserType = properties.getProperty("browser").toLowerCase().trim();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        return browserType;
+    }
     public static void cleanupDriver(){
         webDriverThreadLocal.get().quit();
         webDriverThreadLocal.remove();
