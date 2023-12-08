57edc31ec1ba5b1fe8a734d45692623f34bf60de
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2023-12-08 14:35:39 +0100

    added sendKeys method

--- src/main/java/pageObjects/Base_PO.java
+++ src/main/java/pageObjects/Base_PO.java
@@ -2,7 +2,13 @@ package pageObjects;
 
 import driver.DriverFactory;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Base_PO {
     public Base_PO(){
@@ -18,5 +24,10 @@ public class Base_PO {
 
     public String getRandomNumber(int length){
         return RandomStringUtils.randomNumeric(length);
+    }
+
+    public void sendKeys(WebElement webElement, String textTotype){
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(webElement)).sendKeys(textTotype);
     }
 }
