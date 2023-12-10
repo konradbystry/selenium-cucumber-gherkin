1012d03fbd7d62450b82e19da3393054bd4b8e1d
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2023-12-10 23:10:08 +0100

    added selectors & created methods

--- src/main/java/pageObjects/Login_PO.java
+++ src/main/java/pageObjects/Login_PO.java
@@ -1,8 +1,35 @@
 package pageObjects;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Login_PO extends Base_PO{
+
+    @FindBy(id = "text")
+    private WebElement username_TextField;
+    @FindBy(id = "password")
+    private WebElement password_TextField;
+    @FindBy(id = "login-button")
+    private WebElement login_Button;
 
     public Login_PO(){
         super();
+    }
+
+    public void navigateTo_WebDriverUniversity_Login_Page(){
+        String loginPageUrl = "http://webdriveruniversity.com/Login-Portal/index.html?";
+        navigateTo_URL(loginPageUrl);
+    }
+
+    public void setUsername(String username){
+        sendKeys(username_TextField, username);
+    }
+
+    public void setPassword(String password){
+        sendKeys(password_TextField, password);
+    }
+
+    public void clickOn_Login_Button(){
+        waitForWebElementAndClick(login_Button);
     }
 }
