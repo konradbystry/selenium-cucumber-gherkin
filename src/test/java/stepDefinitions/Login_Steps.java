10fca5bbebf050ee69da1ca6f84a4134e3f678c0
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2023-12-10 23:09:54 +0100

    implemented login page object

--- src/test/java/stepDefinitions/Login_Steps.java
+++ src/test/java/stepDefinitions/Login_Steps.java
@@ -3,35 +3,36 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import pageObjects.Base_PO;
+import pageObjects.Login_PO;
 
 public class Login_Steps extends Base_PO {
 
     private WebDriver driver = getDriver();
 
+    private Login_PO login_po;
+
+    public Login_Steps(Login_PO login_po){
+        this.login_po = login_po;
+    }
+
     @Given("I access the webdriver university login page")
     public void i_access_the_webdriver_university_login_page() {
-        String loginPageUrl = "http://webdriveruniversity.com/Login-Portal/index.html?";
-        navigateTo_URL(loginPageUrl);
+        login_po.navigateTo_WebDriverUniversity_Login_Page();
     }
     @When("I enter a username {word}")
     public void i_enter_a_username(String username) {
-        WebElement loginInput = driver.findElement(By.id("text"));
-        sendKeys(loginInput, username);
+        login_po.setUsername(username);
     }
     @When("I enter a password {word}")
     public void i_enter_a_password(String password) {
-        WebElement passwordInput = driver.findElement(By.id("password"));
-        sendKeys(passwordInput, password);
+        login_po.setPassword(password);
     }
     @When("I click on the login button")
     public void i_click_on_the_login_button() {
-        WebElement loginButton = driver.findElement(By.id("login-button"));
-        loginButton.click();
+        login_po.clickOn_Login_Button();
     }
     @Then("I should be presented with a successful login message")
     public void i_should_be_presented_with_a_successful_login_message() {
