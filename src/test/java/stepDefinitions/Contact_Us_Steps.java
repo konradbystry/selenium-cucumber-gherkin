e6adc787cab12932af2be47a055c4d87628cf477
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2023-12-11 14:10:55 +0100

    implemented contact us page object

--- src/test/java/stepDefinitions/Contact_Us_Steps.java
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -4,13 +4,10 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import pageObjects.Base_PO;
+import pageObjects.Contac_Us_PO;
+
 import java.time.Duration;
 
 
@@ -19,66 +16,55 @@ public class Contact_Us_Steps extends Base_PO {
 
     private WebDriver driver = getDriver();
 
+    private Contac_Us_PO contac_us_po;
+    public Contact_Us_Steps (Contac_Us_PO contac_us_po){
+        this.contac_us_po = contac_us_po;
+    }
+
 
     @Given("I access the webdriver university contact page")
     public void i_access_the_webdriver_university_contact_page() {
-        String contactPageUrl = "http://webdriveruniversity.com/Contact-Us/contactus.html";
-        navigateTo_URL(contactPageUrl);
+        contac_us_po.navigateTo_WebDriverUniversity_Contact_Us_Page();
     }
     @When("I enter a unique first name")
     public void i_enter_a_unique_first_name() {
-        WebElement firstNameInput = driver.findElement(By.cssSelector("input[name=\"first_name\"]"));
-        sendKeys(firstNameInput, "AutoFN" + getRandomNumber(5));
+        contac_us_po.setUnique_FirstName();
     }
     @And("I enter a unique last name")
     public void i_enter_a_unique_last_name() {
-        WebElement lastNameInput = driver.findElement(By.cssSelector("input[name=\"last_name\"]"));
-        sendKeys(lastNameInput, "AutoLN" + getRandomNumber(5));
+       contac_us_po.setUnique_LastName();
     }
     @And("I enter a unique email address")
     public void i_enter_a_unique_email_address() {
-        String email = "test" + getRandomNumber(5) + "@example.com";
-        WebElement emailInput = driver.findElement(By.cssSelector("input[name=\"email\"]"));
-        sendKeys(emailInput, email);
+        contac_us_po.setUnique_Email();
     }
     @And("I enter a unique comment")
     public void i_enter_a_unique_comment() {
-        WebElement commentTextarea = driver.findElement(By.cssSelector("textarea[name=\"message\"]"));
-        sendKeys(commentTextarea, "AutoC" + getRandomNumber(5));
+        contac_us_po.setUnique_Comment();
     }
     @And("I click on the submit button")
     public void i_click_on_the_submit_button() {
-        WebElement submitButton = driver.findElement(By.cssSelector("input[value=\"SUBMIT\"]"));
-        submitButton.click();
+        contac_us_po.clickOn_SubmitButton();
     }
     @Then("I should be presented with a successful contact us submission message")
     public void i_should_be_presented_with_a_successful_contact_us_submission_message() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        String submissionMessageTextExpected = "Thank You for your Message!";
-        WebElement submissionMessage = driver.findElement(By.cssSelector("#contact_reply > h1"));
-        wait.until(ExpectedConditions.visibilityOf(submissionMessage));
-        String submissionMessageTextActual = submissionMessage.getText();
-        Assert.assertTrue(submissionMessageTextActual.equalsIgnoreCase(submissionMessageTextExpected));
+        contac_us_po.validateSuccessful_SubmissionMessage();
     }
 
     @When("I enter a specific first name {word}")
     public void i_enter_a_specific_first_name(String firstName) {
-        WebElement firstNameInput = driver.findElement(By.cssSelector("input[name=\"first_name\"]"));
-        sendKeys(firstNameInput, firstName);
+        contac_us_po.setSpecific_FirstName(firstName);
     }
     @When("I enter a specific last name {word}")
     public void i_enter_a_specific_last_name(String lastName) {
-        WebElement lastNameInput = driver.findElement(By.cssSelector("input[name=\"last_name\"]"));
-        sendKeys(lastNameInput, lastName);
+        contac_us_po.setSpecific_LastName(lastName);
     }
     @When("I enter a specific email address {word}")
     public void i_enter_a_specific_email_address(String email) {
-        WebElement emailInput = driver.findElement(By.cssSelector("input[name=\"email\"]"));
-        sendKeys(emailInput, email);
+        contac_us_po.setSpecific_Email(email);
     }
     @When("I enter a specific comment {string}")
     public void i_enter_a_specific_comment(String comment) {
-        WebElement commentTextarea = driver.findElement(By.cssSelector("textarea[name=\"message\"]"));
-        sendKeys(commentTextarea, comment);
+        contac_us_po.setSpecific_Comment(comment);
     }
 }
