9020b83491ed37e2dee63257e965bea4dd87c5cd
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2023-12-11 14:30:33 +0100

    added global vars class

---
+++ src/main/java/utils/Global_Vars.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Global_Vars {
+    public static final int DEFAULT_EXPLICIT_WAIT_TIME = 10;
+    public static final String APP_URL = "http://webdriveruniversity.com";
+}
